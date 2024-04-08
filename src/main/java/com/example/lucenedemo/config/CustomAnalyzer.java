aa6222d9fb472793509b1cc1a8c226af19eed60d
Vishal <vishalkumar437@gmail.com>
2024-04-08 13:06:59 +0530

    adding ngram to analyzer

--- src/main/java/com/example/lucenedemo/config/CustomAnalyzer.java
+++ src/main/java/com/example/lucenedemo/config/CustomAnalyzer.java
@@ -3,13 +3,17 @@ package com.example.lucenedemo.config;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.LowerCaseFilter;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.ngram.EdgeNGramTokenFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 
 public class CustomAnalyzer extends Analyzer {
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
         StandardTokenizer tokenizer = new StandardTokenizer();
-        TokenStream tokenStream = new LowerCaseFilter(tokenizer);
+        TokenStream tokenStream = tokenizer;
+        tokenStream = new EdgeNGramTokenFilter(tokenStream, 3, 8, true);
+        tokenStream = new LowerCaseFilter(tokenStream);
+//        tokenStream = new PhoneticFilter(tokenStream, new DoubleMetaphone(), false);
         return new TokenStreamComponents(tokenizer, tokenStream);
     }
 }
